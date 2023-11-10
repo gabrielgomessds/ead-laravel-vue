9e82ad6a9a928084ec9ece823088b6804dee8428
gabrielgomessds <gabrielgomessdasilva13@gmail.com>
2023-11-10 09:48:48 -0300

    Create Repository

---
+++ laravel-api-ead/app/Http/Controllers/Api/CourseController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Resources\CourseResource;
+use App\Repositories\CourseRepository;
+
+class CourseController extends Controller
+{
+    protected $repository;
+
+    public function __construct(CourseRepository $courseRespository)
+    {
+        $this->repository = $courseRespository;
+    }
+    
+    public function index()
+    {
+
+        return CourseResource::collection($this->repository->getAllCourses());
+    }
+
+    public function show($id)
+    {
+
+        return new CourseResource($this->repository->getCourse($id));;
+    }
+}
