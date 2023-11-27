c874023b3e120f44a8ce40a7e65bb79a43546bd7
gabrielgomessds <gabrielgomessdasilva13@gmail.com>
2023-11-27 11:27:31 -0300

    Create Template

---
+++ vue-3-ead/src/views/supports/MySupports.vue
@@ -0,0 +1,15 @@
+
+
+<template>
+    <div class="home">
+      Minhas dúvidas
+    </div>
+  </template>
+  
+  <script>
+    
+    export default {
+      name: 'MySupports',
+      
+    }
+  </script>
